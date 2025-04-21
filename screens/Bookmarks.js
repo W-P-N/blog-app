b5803355dbd8ed76376709da3a462e8e957cfefa
Parth <parth.wani@mitaoe.ac.in>
2025-04-21 16:25:52 -0500

    Hurry commit

---
+++ screens/Bookmarks.js
@@ -0,0 +1,28 @@
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import { blogData } from "../data/data";
+import BlogCard from "../components/BlogCard";
+
+function Bookmarks() {
+    const bookmarkedData = blogData.filter((blog) => blog.isBookmarked === true);
+    function renderBlogCard(blogData) {
+        const blog = blogData.item;
+        return (
+            <BlogCard author={blog.author} comments={blog.comments} imageUrl={blog.imageUrl} likes={blog.likes} title={blog.title} />
+        )
+    }
+    return (
+        <View>
+            <FlatList
+                 data={bookmarkedData}
+                 keyExtractor={(blogData) => blogData.id}
+                 renderItem={renderBlogCard}
+            />
+        </View>
+    );
+};
+
+export default Bookmarks;
+
+const styles = StyleSheet.create({
+
+})
